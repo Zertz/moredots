c1d6d6eecb8dd73da2b06fe96e7ea20aac116776
Pier-Luc Gendreau <pierluc@outlook.com>
2021-06-25 22:43:07 -0400

    Update deps, modernize a bit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
-'use strict'
+"use strict";
 
-const isPlainObject = require('lodash.isplainobject')
+const isPlainObject = require("lodash.isplainobject");
 
-{
-  const moredots = function (src, dst, prefix) {
-    dst = typeof dst === 'undefined' ? {} : dst
-    prefix = typeof prefix === 'undefined' ? '' : prefix
-
-    for (var key in src) {
-      if (isPlainObject(src[key])) {
-        moredots(src[key], dst, prefix + key + '.')
-      } else {
-        dst[prefix + key] = src[key]
-      }
+module.exports = function moredots(src, dst = {}, prefix = "") {
+  for (const key in src) {
+    if (isPlainObject(src[key])) {
+      moredots(src[key], dst, prefix + key + ".");
+    } else {
+      dst[prefix + key] = src[key];
     }
-
-    return dst
   }
 
-  module.exports = moredots
-}
+  return dst;
+};
